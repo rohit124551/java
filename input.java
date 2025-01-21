70300e1d83da857dcc8b7912b5c3554e7efa4a70
Rohit Kumar Ranjan <rohit124551@gmail.com>
2025-01-21 14:44:13 +0530

    added

--- input.java
+++ input.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;
 public class input {
     public static void main(String[] args){
-        
+        Scanner obj =new Scanner(System.in);
+        int a=obj.nextInt();
+        System.out.println("value of a:"+a);
     }
     
 }
---
+++ wrapper.java
@@ -0,0 +1,9 @@
+public class wrapper {
+    public static void main(String[] args){
+        int i=1;
+        integer
+        String str="hii";
+        System.out.println(str.length());
+        System.out.println());
+    }
+}
