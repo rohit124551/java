95f535eac0f88f7fcc14907930a8024c0fef2ec0
Rohit Kumar Ranjan <rohit124551@gmail.com>
2025-01-22 15:36:29 +0530

    added

---
+++ j3.java
@@ -0,0 +1,14 @@
+public class j3 {
+    public static void main(String[] args){
+        int a=1;
+        if (a > 0){
+            System.out.println("value is greater than 0");
+
+        }else if(a == 1){
+            System.out.println("value is equal to 1");
+
+        }else{
+            System.out.println("value is less than 0");
+        }
+    }
+}
--- wrapper.java
+++ wrapper.java
@@ -1,9 +1,9 @@
 public class wrapper {
     public static void main(String[] args){
         int i=1;
-        integer
-        String str="hii";
+        Integer j=1;
+        String str="hiii";
         System.out.println(str.length());
-        System.out.println());
+        System.out.println(j.floatValue());
     }
 }
