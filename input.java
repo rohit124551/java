5165e5fa9c363713b00a53068f36de337a811523
Rohit Kumar Ranjan <rohit124551@gmail.com>
2025-01-29 11:53:39 +0530

    input neo prct

--- input.java
+++ input.java
@@ -3,8 +3,10 @@ public class input {
     public static void main(String[] args){
         Scanner obj =new Scanner(System.in);
         int a=obj.nextInt();
+        int b=obj.nextInt();
         System.out.println("value of a:"+a);
-        
+        System.out.println("value of a:"+b);
+    
     }
     
 }
