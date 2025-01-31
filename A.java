f0a6afb290a8c9df26a155362702f427449fd189
Rohit Kumar Ranjan <rohit124551@gmail.com>
2025-01-31 15:29:31 +0530

    class and object topics

---
+++ A.java
@@ -0,0 +1,25 @@
+// class A {
+//     public static void aa(int... n){
+//         for (int i:n){
+//         System.out.println(i);}
+//     }
+//     public static void main(String[] args){
+//         A a=new A();
+//         aa(1,2,3,4,5);
+//     }
+
+    
+// }
+
+class A{
+    public static void aa(String s,int... n){
+        System.out.println(s);
+        for(int i:n){
+            System.out.println(i);}
+
+        }
+        public static void main(String[] args) {
+            A a=new A();
+            aa("Hiiii",1,2,3,4,5);
+        }
+    }
