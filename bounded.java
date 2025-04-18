869fba7e8b54f4e16ea460be26ed5a4c75dba0ee
Rohit Kumar Ranjan <rohit124551@gmail.com>
2025-04-18 16:22:13 +0530

    bounded

---
+++ bounded.java
@@ -0,0 +1,19 @@
+public class bounded {
+    void a1(List<?> 1){
+        
+    }
+    void a2(List<?> extends Number> l){
+        for
+    }
+    void a3(List<?> super Integers> l){
+        
+    }
+}
+
+class B{
+    public static void main(String[] args) {
+        List<Integers> l1=List.of(1,2,3,4);
+
+        A a=new A();
+    }
+}
